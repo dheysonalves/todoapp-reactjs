d117951355cc801f91dca9cc7e86f260733b621d
Dheyson Alves <dheyson.lucas@hotmail.com>
2020-03-01 16:01:37 -0300

    Adding the frontend structure

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Header from '../template/Header';
+
+export default props => (
+  <div>
+    <Header name="About" small="Something" />
+
+    <h2>Our History</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi illo tempore debitis modi, eveniet hic ad asperiores accusamus, nisi ducimus reprehenderit. Quas amet inventore reprehenderit consequuntur nisi repellendus cupiditate possimus!</p>
+  </div>
+)
---
+++ frontend/src/todo/todo.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import Header from '../template/Header';
+
+export default class todo extends Component {
+  render() {
+    return (
+    <div>
+      <Header name="Todo" small="Sign up"/>
+    </div>
+    );
+  }
+}
