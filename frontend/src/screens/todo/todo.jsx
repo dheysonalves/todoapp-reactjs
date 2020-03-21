b418aa60dfc06472903a55349d68379863b00b54
Dheyson Alves <dheyson.lucas@hotmail.com>
2020-03-21 17:58:23 -0300

    Component to add a simple value

--- frontend/src/screens/todo/todo.jsx
+++ frontend/src/screens/todo/todo.jsx
@@ -5,11 +5,30 @@ import TodoList from './todoList';
 import TodoForm from './todoForm';
 
 export default class todo extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			description: '',
+			list: []
+		}
+		this.handleChange = this.handleChange.bind(this);
+		this.handleApp = this.handleApp.bind(this);
+	}
+
+	handleChange(e) {
+		this.setState({...this.state, description: e.target.value})
+	}
+
+	handleApp() {
+		console.log(this);
+	}
+
   render() {
     return (
     <div>
       <Header name="Todo" small="Sign up"/>
-      <TodoForm />
+      <TodoForm handle={this.handleApp} description={this.state.description} handleChange={this.handleChange}/>
       <TodoList />
     </div>
     );
--- frontend/src/screens/todo/todoForm.jsx
+++ frontend/src/screens/todo/todoForm.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import Grid from '../../components/template/Grid';
 import IconButton from '../../components/template/IconButton';
 
-const todoForm = () => {
+const todoForm = (props) => {
 	return (
-
 		<div role='form' className='todoForm'>
 			<Grid cols="12 9 19">
-        <input className="form-control" type="text" name="" placeholder="Adicione uma tarefa" id=""/>
+        <input className="form-control" type="text" value={props.description} placeholder="Adicione uma tarefa" id="" onChange={props.handleChange}/>
       </Grid>
 		<Grid cols="12 3 2">
-			<IconButton hide={false} style="primary" icon="plus" />
+			<IconButton hide={false} onClick={props.handle} style="primary" icon="plus" />
 		</Grid>
 		</div>
 	);
