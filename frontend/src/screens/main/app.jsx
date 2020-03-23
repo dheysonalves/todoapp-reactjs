c3709ff3a4c0b6ff06c42deaa43489ea792f1049
Dheyson Alves <dheyson.lucas@hotmail.com>
2020-03-22 23:58:13 -0300

    Fix some mistaken imports

--- frontend/src/screens/main/app.jsx
+++ frontend/src/screens/main/app.jsx
@@ -1,10 +1,10 @@
 import "modules/bootstrap/dist/css/bootstrap.min.css";
 import "modules/font-awesome/css/font-awesome.min.css";
-import '../../components/template/custom.css';
+import '../../styles/custom.css';
 
 import React from 'react';
 
-import Menu from '../../components/template/Menu';
+import Menu from '../../components/Menu/Menu';
 import Routes from './routes';
 
 export default props => (
